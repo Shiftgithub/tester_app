ffc5e0e6dd5379541b8947de93a1432b0dc9e9b6
Shiftgithub <sajibadhikery@gmail.com>
2020-12-04 13:43:27 +0600

    #5

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hello Duni bashi'),
-));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tester APP'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Hello Testers'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('Click'),
+        ),
+      ),
+    ));
