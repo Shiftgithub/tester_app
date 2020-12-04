85e752cee6f84b45bf6982587cd6f77d1326c888
Shiftgithub <sajibadhikery@gmail.com>
2020-12-04 15:13:12 +0600

    #6

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text('Tester APP'),
           centerTitle: true,
+          backgroundColor: Colors.orange[600],
         ),
         body: Center(
-          child: Text('Hello Testers'),
+          child: Text(
+              'Hello Tester',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'indieFlower',
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
+          onPressed: () {},
           child: Text('Click'),
+          backgroundColor: Colors.orange[600],
         ),
       ),
     ));
