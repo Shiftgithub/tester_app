3518891a5523b64ee31b8b2da6575772da7aed20
Shiftgithub <sajibadhikery@gmail.com>
2020-12-04 15:53:20 +0600

    #7

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Tester APP'),
-          centerTitle: true,
-          backgroundColor: Colors.orange[600],
-        ),
-        body: Center(
-          child: Text(
-              'Hello Tester',
-            style: TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'indieFlower',
-            ),
+      home: Home(),
+    ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tester APP'),
+        centerTitle: true,
+        backgroundColor: Colors.orange[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hello Tester',
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'indieFlower',
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Click'),
-          backgroundColor: Colors.orange[600],
-        ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text(
+          'click Me',
+          style: TextStyle(
+            fontSize: 15.0,
+          ),
+        ),
+        backgroundColor: Colors.orange[600],
+      ),
+    );
+  }
+}
+
+
