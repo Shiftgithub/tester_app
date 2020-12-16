925d16e249cb937813e1e3260463941f00715b9f
Shiftgithub <sajibadhikery@gmail.com>
2020-12-16 20:02:27 +0600

    #11 CrossAxisAlignment Center

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class Home extends StatelessWidget {
       ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Text('Hello , Testers'),
           FlatButton(
